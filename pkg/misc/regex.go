bc9d81bd8eacd7af7377400ccf81d65ba56e060f
Mustafa Gezen <mustafa@gezen.no>
2022-04-23 22:56:39 +0200

    Strict branch mode should still include module streams

--- pkg/misc/regex.go
+++ pkg/misc/regex.go
@@ -10,6 +10,8 @@ func GetTagImportRegex(pd *data.ProcessData) *regexp.Regexp {
 	branchRegex := fmt.Sprintf("%s%d%s", pd.ImportBranchPrefix, pd.Version, pd.BranchSuffix)
 	if !pd.StrictBranchMode {
 		branchRegex += "(?:.+|)"
+	} else {
+		branchRegex += "(?:-stream-.+|)"
 	}
 	regex := fmt.Sprintf("refs/tags/(imports/(%s)/(.*))", branchRegex)
 
