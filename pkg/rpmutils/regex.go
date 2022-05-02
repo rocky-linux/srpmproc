f7017a9eacd405da335839b5622f1ca915d0d3ce
Mustafa Gezen <mustafa@gezen.no>
2022-05-02 10:39:42 +0200

    Fix NVR regex for versions with a percentage, and fix dist regex for any EL

--- pkg/rpmutils/regex.go
+++ pkg/rpmutils/regex.go
@@ -3,9 +3,9 @@ package rpmutils
 import "regexp"
 
 var (
-	Nvr        = regexp.MustCompile("^(\\S+)-([\\w.]+)-(\\w+(?:\\.[\\w+]+)+?)(?:\\.(\\w+))?(?:\\.rpm)?$")
+	Nvr        = regexp.MustCompile("^(\\S+)-([\\w~%.]+)-(\\w+(?:\\.[\\w+]+)+?)(?:\\.(\\w+))?(?:\\.rpm)?$")
 	epoch      = regexp.MustCompile("(\\d+):")
 	module     = regexp.MustCompile("^(.+)-(.+)-([0-9]{19})\\.((?:.+){8})$")
-	dist       = regexp.MustCompile("(\\.el8(?:(?:_\\d)|))")
+	dist       = regexp.MustCompile("(\\.el\\d(?:_\\d|))")
 	moduleDist = regexp.MustCompile("\\.module.+$")
 )
